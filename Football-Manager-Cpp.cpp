b1311405dc7d7cb39f585cd919209952ef795de3
PatrykIT <the.last.of.us.08.08@gmail.com>
2016-01-03 23:44:17 +0100

    Removed old C code

--- Football-Manager-Cpp.cpp
+++ Football-Manager-Cpp.cpp
@@ -24,8 +24,6 @@ void Start()
 		club->Add_Player_to_Club(*players_1[i]);
 	}
 
-//--------------------------CREATING 7 RIVAL CLUBS--------------------------- (From old C version of this program, will edit this later)
-
 	Club *club_2 = new Club;
 	struct Player *players_2[number_of_players];
 
@@ -53,44 +51,6 @@ void Start()
 		club_4->Add_Player_to_Club(*players_4[i]);
 	}
 
-	/*struct Club *club_5 = Create_Club();
-	struct Player *players_5[number_of_players];
-
-	for (i = 0; i < number_of_players; ++i)
-	{
-		players_5[i] = Create_Player();
-		Add_Player_to_Club(&players_5[i], &club_5);
-	}
-
-	struct Club *club_6 = Create_Club();
-	struct Player *players_6[number_of_players];
-
-	for (i = 0; i < number_of_players; ++i)
-	{
-		players_6[i] = Create_Player();
-		Add_Player_to_Club(&players_6[i], &club_6);
-	}
-
-	struct Club *club_7 = Create_Club();
-	struct Player *players_7[number_of_players];
-
-	for (i = 0; i < number_of_players; ++i)
-	{
-		players_7[i] = Create_Player();
-		Add_Player_to_Club(&players_7[i], &club_7);
-	}
-
-	struct Club *club_8 = Create_Club();
-	struct Player *players_8[number_of_players];
-
-	for (i = 0; i < number_of_players; ++i)
-	{
-		players_8[i] = Create_Player();
-		Add_Player_to_Club(&players_8[i], &club_8);
-	}*/
-
-
-//-----------------------------------------------------------------------------
 
 	Table *table = new Table;
 	table->Add_Club_to_Table(&club);
@@ -98,23 +58,17 @@ void Start()
 	table->Add_Club_to_Table(&club_3);
 	table->Add_Club_to_Table(&club_4);
 
-	/*Add_Club_to_Table(&club_5, &table);
-	Add_Club_to_Table(&club_6, &table);
-	Add_Club_to_Table(&club_7, &table);
-	Add_Club_to_Table(&club_8, &table);*/
 
 	club->Set_Tactics();
 	club_2->Set_Tactics();
 	club_3->Set_Tactics();
 	club_4->Set_Tactics();
 
-	/*Set_Tactics(&club_5);
-	Set_Tactics(&club_6);
-	Set_Tactics(&club_7);
-	Set_Tactics(&club_8);*/
 
 	table->Schedule_Season();
 
+	table->Play_Round();
+	table->Play_Round();
 	table->Play_Round();
 
 	delete club; delete club_2; delete club_3; delete club_4;
@@ -127,15 +81,15 @@ void Start()
  * Dokończyć  Schedule_Rounds().
  * Dokończyć choice = 2 w Play_Round() - zrobić dogrywanie meczy w przyszłym czasie.
  * Transform to smart pointers.
- * Set_Tactics - change to QuickSort
  * Try to change colours in console.
- * Add age as a factor to improving skills. Add declining of older players with time passing.
+ * Make it user-friendly.
+ * Finish Season_Finished() - create top goalscorers etc.
+ * Reformat Save_History() - it should return a string.
  */
 
 int main()
 {
 	srand(time(NULL));
-	calendar->Print_Date();
 	Start();
 
 
