f1a466c60f765b05e236daffc39e44af21eaabc2
PatrykIT <the.last.of.us.08.08@gmail.com>
2016-02-24 19:44:07 +0100

    Constructor moved to the top.

--- Calendar.cpp
+++ Calendar.cpp
@@ -3,6 +3,22 @@
 #include "Attributes.h"
 #include "Clubs.h"
 
+Calendar::Calendar()
+{
+	time_t raw_time;
+	time(&raw_time); //returns the time since the Epoch (00:00:00 UTC, January 1, 1970), measured in seconds.
+
+	struct tm *my_date = localtime(&raw_time); //struct tm - this is a structure used to hold the time and date.
+
+
+	year = my_date->tm_year + 1900; //The number of years since 1900
+	month = my_date->tm_mon + 1; //Month, range 0 to 11. Adding +1 for better readability.
+	day_of_a_month = my_date->tm_mday;
+	day_of_a_week = my_date->tm_wday;
+	hour = my_date->tm_hour;
+	minute = my_date->tm_min;
+	seconde = my_date->tm_sec;
+}
 
 void Calendar::Set_Hour() //Used by History class to save messages - it update's the time, without updating the date, because date is constantly modified by Travel_Calendar().
 {
@@ -52,22 +68,6 @@ void Calendar::Travel_Calendar(int days)
 }
 
 
-Calendar::Calendar()
-{
-	time_t raw_time;
-	time(&raw_time); //returns the time since the Epoch (00:00:00 UTC, January 1, 1970), measured in seconds.
-
-	struct tm *my_date = localtime(&raw_time); //struct tm - this is a structure used to hold the time and date.
-
-
-	year = my_date->tm_year + 1900; //The number of years since 1900
-	month = my_date->tm_mon + 1; //Month, range 0 to 11. Adding +1 for better readability.
-	day_of_a_month = my_date->tm_mday;
-	day_of_a_week = my_date->tm_wday;
-	hour = my_date->tm_hour;
-	minute = my_date->tm_min;
-	seconde = my_date->tm_sec;
-}
 
 
 Calendar& Calendar::get()
